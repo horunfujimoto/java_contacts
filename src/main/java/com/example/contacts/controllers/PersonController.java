b22d0340b24484a49774ff31cbfde0b72dd57f0f
HorunFujimoto <horun.fujimoto@weserve.co.jp>
2025-01-24 14:36:14 +0900

    Modelファイルの作成

--- src/main/java/com/example/contacts/controllers/PersonController.java
+++ src/main/java/com/example/contacts/controllers/PersonController.java
@@ -1,22 +1,22 @@
 package com.example.contacts.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.contacts.models.Person;
 
 @Controller
 public class PersonController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(@ModelAttribute Person person) {
         return "person/index";
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam String name, Model model) {
-        model.addAttribute("name", name);
+    public String create(@ModelAttribute Person person){
         return "person/create";
     }
 }
