216b9ac6166879e9fdf4c52951276d95c9d698b2
horunfujimoto <hello.horurun@gmail.com>
2025-01-28 12:06:39 +0900

    バリデーションエラーメッセージの表示

--- src/main/java/com/example/contacts/controllers/PersonController.java
+++ src/main/java/com/example/contacts/controllers/PersonController.java
@@ -1,6 +1,8 @@
 package com.example.contacts.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,12 @@ public class PersonController {
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute Person person){
+    // エラーの内容はBindingResultに入る
+    public String create(@Validated @ModelAttribute Person person, BindingResult result){
+        // バリデーションエラーがある場合はindex.htmlを表示
+        if(result.hasErrors()){
+            return "person/index";
+        }
         return "person/create";
     }
 }
--- src/main/java/com/example/contacts/models/Person.java
+++ src/main/java/com/example/contacts/models/Person.java
@@ -1,10 +1,28 @@
 package com.example.contacts.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Person {
+  // 文字列の入力必須のバリデーション
+  @NotBlank
+  // 文字列の長さが指定の文字数
+  @Size(max=120)
   private String name;
+
+  @NotNull
+  @Min(0)
+  @Max(120)
   private Integer age;
+
+  @NotBlank
+  @Email
+  @Size(max = 254)
   private String email;
 }
